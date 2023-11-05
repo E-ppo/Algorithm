fb06703007dda2477e8fe5cf94ef4c3fb6f878de
epppo <amorosso@kakao.com>
2023-11-05 23:09:54 +0900

    코테 20일차

--- CodingTest/백준/25206.js
+++ CodingTest/백준/25206.js
@@ -1,7 +1,33 @@
-// split은 객체에만 사용가능하니 한번 사용후에는 배열이 되므로 map을 사용해 중복split
-const input = require("fs")
-  .readFileSync(0)
+const fs = require("fs")
+const filePath = process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt"
+const input = fs
+  .readFileSync(filePath)
   .toString()
   .trim()
   .split("\n")
-  .map((x) => x.split(" "))
+  .map((i) => i.split(" "))
+
+const creditTable = {
+  "A+": 4.5,
+  A0: 4.0,
+  "B+": 3.5,
+  B0: 3.0,
+  "C+": 2.5,
+  C0: 2.0,
+  "D+": 1.5,
+  D0: 1.0,
+  F: 0.0,
+}
+
+const solution = (input) => {
+  let total = 0
+  let average = 0
+  for (i = 0; i < input.length; i++) {
+    if (input[i][2] === "P") continue
+    average += Number(input[i][1])
+    total += Number(input[i][1]) * Number(creditTable[input[i][2]])
+  }
+  return total / average
+}
+
+console.log(solution(input))
--- CodingTest/백준/2566.js
+++ CodingTest/백준/2566.js
@@ -11,8 +11,8 @@ let max = 0
 let y = 0
 let x = 0
 
-for (let i = 0; i < 9; i++) {
-  for (let j = 0; j < 9; j++) {
+for (i = 0; i < 9; i++) {
+  for (j = 0; j < 9; j++) {
     if (max < input[i][j]) {
       max = input[i][j]
       y = i + 1

9804f14531aa108fd8ff4c2284e602117337ab45
E-ppo <amorosso@kakao.com>
2023-11-10 23:14:03 +0900

    Delete 백준/2562.js

--- 백준/2562.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs")
-const filePath = process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt"
-const input = fs.readFileSync(filePath).toString().trim().split("\n").map(Number) // 배열의 각 요소를 숫자로 변환
-
-const max = Math.max(...input)
-const index = input.indexOf(max)
-console.log(max)
-console.log(index + 1)

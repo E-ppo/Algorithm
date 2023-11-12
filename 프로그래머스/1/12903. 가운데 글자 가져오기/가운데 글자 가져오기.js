e585247f3e8cba2ad91ba1fbc496d232e95687d8
E-ppo <amorosso@kakao.com>
2023-11-12 22:09:24 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,10 +1,12 @@
 function solution(s) {
-    var answer = '';
-    
-    if (s.length % 2 == 0 ) { 
-        answer = s[s.length / 2 - 1] + s[s.length / 2];
-    } else {
-        answer = s[Math.floor(s.length/2)]; 
-    }
-    return answer;
+  let answer
+  let mid = Math.floor(s.length / 2)
+
+  if (s.length % 2 !== 0) {
+    answer = s[mid]
+  } else {
+    answer = s[mid - 1] + s[mid]
+  }
+
+  return answer
 }

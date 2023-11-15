9aeff141e6fdfa3e226b8df7e1fa3ab7b31745d6
epppo <amorosso@kakao.com>
2023-11-15 22:00:15 +0900

    코테 28일차

--- 백준/note.js
+++ 백준/note.js
@@ -2,8 +2,4 @@ const fs = require("fs")
 const filePath = process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt"
 const input = fs.readFileSync(filePath).toString().trim().split("\n")
 
-// console.log(input[1].map((i) => Number(i)))
-const numbers = input[1].split(" ").map((i) => Number(i))
-
-const answer = numbers.filter((i, idx) => numbers.indexOf(i) === idx).sort((a, b) => a - b)
-console.log(answer.join(" "))
+console.log(input)

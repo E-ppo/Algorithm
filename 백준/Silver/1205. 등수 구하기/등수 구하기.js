16ebacd7a4344ca5fa7b3c3ab2fde78a52e87eb7
E-ppo <amorosso@kakao.com>
2023-11-23 00:06:49 +0900

    [Silver IV] Title: 등수 구하기, Time: 124 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Silver/1205. 등수 구하기/등수 구하기.js
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.js
@@ -2,18 +2,18 @@ const fs = require("fs")
 const filePath = process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt"
 const input = fs.readFileSync(filePath).toString().trim().split("\n")
 
-const nsp = input[0].split(" ").map(Number)
-if (nsp[0] === 0) return console.log(1)
+const [n, tesuS, p] = input[0].split(" ").map(Number)
+if (n === 0) return console.log(1)
 const score = input[1].split(" ").map(Number)
 
 let tesuRank = 1
 
 for (i = 0; i < score.length; i++) {
-  if (score[i] > nsp[1]) {
+  if (score[i] > tesuS) {
     tesuRank++
-  } else if (score[nsp[2] - 1] >= nsp[1]) {
+  } else if (score[p - 1] >= tesuS) {
     return console.log(-1)
   }
 }
 
-console.log(nsp[2] < tesuRank ? -1 : tesuRank)
+console.log(p < tesuRank ? -1 : tesuRank)
